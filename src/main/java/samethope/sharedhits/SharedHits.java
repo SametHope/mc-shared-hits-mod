f4eeb3825b0774c35914669d81e760e371a156d4
SametHope <samethgoxx@gmail.com>
2025-03-01 19:52:07 +0300

    Initial commit

---
+++ src/main/java/samethope/sharedhits/SharedHits.java
@@ -0,0 +1,20 @@
+package samethope.sharedhits;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.world.GameRules;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SharedHits implements ModInitializer {
+	public static final String MOD_ID = "shared-hits";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static GameRules.Key<GameRules.BooleanRule> SHARE_HITS;
+
+	@Override
+	public void onInitialize() {
+		SHARE_HITS = GameRuleRegistry.register("shareHits", GameRules.Category.PLAYER, GameRuleFactory.createBooleanRule(true));
+		LOGGER.info("Shared hits initialized!");
+	}
+}
